fc0dc8d7b12b24d04919bfe4e9ac23cd59126eff
vkrasilschik <vladimir.krasilschik@jugru.org>
2025-05-21 10:39:31 +0300

    - фикс тестов - так более честно

--- src/test/java/ru/spb/kupchinolab/jvmday2025/dining_philosophers/_011_junit5_tests_vertx/VirtualVerticlePhilosophersTest.java
+++ src/test/java/ru/spb/kupchinolab/jvmday2025/dining_philosophers/_011_junit5_tests_vertx/VirtualVerticlePhilosophersTest.java
@@ -36,7 +36,7 @@ public class VirtualVerticlePhilosophersTest {
         System.out.println("all verticles deployed at " + Instant.now());
         vertx.eventBus().consumer("max_eat_attempts_has_reached", msg -> {
             System.out.println("finish eating at " + Instant.now() + ", msg: " + msg.body());
-            maxEatAttemptsHasReached.flag();
+            vertx.close().onComplete(_ -> maxEatAttemptsHasReached.flag());
         });
         System.out.println("start  eating at " + Instant.now());
         vertx.eventBus().publish("start_barrier", "Go-go-go!");
